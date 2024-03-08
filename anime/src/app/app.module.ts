974ca1e74bb94865ea992bb40508fdd8fdf88ceb
daniescobarx <daniela.mollo@sptech.school>
2024-03-08 20:54:06 -0300

    add

--- anime/src/app/app.module.ts
+++ anime/src/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+//modulos
+import {FormsModule} from "@angular/forms"
+
+//componentes
 import { AppComponent } from './app.component';
 import { SearchAnimeComponent } from './components/search-anime/search-anime.component';
 import { ResultAnimeComponent } from './components/result-anime/result-anime.component';
@@ -16,7 +21,8 @@ import { SelectedAnimeComponent } from './components/selected-anime/selected-ani
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
