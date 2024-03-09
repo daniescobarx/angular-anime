ab8f2f8b6c8348ac9242c9139c401b809739217a
daniescobarx <daniela.mollo@sptech.school>
2024-03-08 23:45:54 -0300

    component resultAnime

--- anime/src/app/components/result-anime/result-anime.component.ts
+++ anime/src/app/components/result-anime/result-anime.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AnimeService } from '../../services/anime.service';
 
 @Component({
   selector: 'app-result-anime',
   templateUrl: './result-anime.component.html',
   styleUrl: './result-anime.component.css'
 })
-export class ResultAnimeComponent {
+export class ResultAnimeComponent implements OnInit {
+  constructor(private AnimeService: AnimeService){
 
+  }
+
+  ngOnInit(): void {
+      this.AnimeService.getResultAnime().subscribe(result => {
+        console.log('componente result-anime',result);
+      })
+  }
 }
